b8f0484bd6c6a2f68d7235c86bfa909e077f6a40
Vladislav Nikolaev <vladisnotlove@gmail.com>
2024-11-04 23:52:49 +0300

    Add size in spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -4,11 +4,19 @@ import "./Spinner.css";
 
 type SpinnerProps = {
 	className?: string;
+	size?: number;
 };
 
-export const Spinner: React.FC<SpinnerProps> = ({ className }) => {
+export const Spinner: React.FC<SpinnerProps> = ({ className, size }) => {
 	return (
-		<div className={classNames("gg-spinner", className)}>
+		<div
+			className={classNames("gg-spinner", className)}
+			style={{
+				...(size && {
+					width: size + "px",
+				}),
+			}}
+		>
 			<svg className="gg-spinner__svg" viewBox="22 22 44 44">
 				<circle
 					className="gg-spinner__circle"
